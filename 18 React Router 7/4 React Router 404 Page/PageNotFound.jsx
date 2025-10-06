eb86ba72d2c4984d8ad5b884665d7597d88d4263
Riyaz Pathan <moh.riyazpathan@gmail.com>
2025-10-07 00:17:05 +0530

    Added routing method for 404 page

---
+++ 18 React Router 7/4 React Router 404 Page/PageNotFound.jsx
@@ -0,0 +1,8 @@
+export default function PageNotFound() {
+  return (
+    <div style={{textAlign:'center'}}>
+      <h1>404 Page</h1>
+      <img style={{width:"400px"}} src="src\404 3.jpg" alt="" />
+    </div>
+  );
+}
