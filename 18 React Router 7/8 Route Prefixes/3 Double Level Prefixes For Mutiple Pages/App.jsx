86b84d68830033f9024513b85879c48b90f0566c
Riyaz Pathan <moh.riyazpathan@gmail.com>
2025-10-12 00:44:36 +0530

    Added Notes and Code for Prefixs in React Router

---
+++ 18 React Router 7/8 Route Prefixes/3 Double Level Prefixes For Mutiple Pages/App.jsx
@@ -0,0 +1,37 @@
+import { Routes, Route, Link, Navigate } from "react-router";
+import Home from "./Home";
+import About from "./About";
+import Login from "./Login";
+import Navbar from "./NewNavbar";
+import PageNotFound from "./PageNotFound";
+import College from "./College";
+import Student from "./Student";
+import Departments from "./Departments";
+import Details from "./Details";
+
+export default function App() {
+  return (
+    <>
+      {/* <Navbar /> */}
+
+      <Routes>
+        <Route element={<Navbar />}>
+          <Route path="/" element={<Home />} />
+          <Route path="en/user">
+            <Route path="/en/user/about" element={<About />} />
+            <Route path="/en/user/login" element={<Login />} />
+          </Route>
+        </Route>
+
+        <Route path="/college" element={<College />}>
+          <Route index element={<Student />} />
+          {/* index will load the content of this route by default */}
+          <Route path="departments" element={<Departments />} />
+          <Route path="details" element={<Details />} />
+        </Route>
+
+        <Route path="/*" element={<Navigate to="/" />} />
+      </Routes>
+    </>
+  );
+}
---
+++ 18 React Router 7/8 Route Prefixes/3 Double Level Prefixes For Mutiple Pages/NewNavbar.jsx
@@ -0,0 +1,19 @@
+import { Link, Outlet } from "react-router";
+import './header.css';
+
+export default function Navbar() {
+  return (<>
+    <div className="header">
+      <Link className="logo" to="/"><h2>Logo</h2></Link>
+      <ul className="nav-links">
+        <li><Link className="link" to="/">Home</Link></li>
+        <li><Link className="link" to="/en/user/about">About</Link></li>
+        <li><Link className="link" to="/college">College</Link></li>
+        <li><Link className="link" to="/en/user/login">Login</Link></li>
+      </ul>
+    </div>
+    <Outlet/>
+    {/* Outlet is use to render the chinl element */}
+  </>
+  );
+}
