a35d6b318666388f01cd2bf0eb97dd4568322446
Riyaz Pathan <moh.riyazpathan@gmail.com>
2025-10-18 09:59:35 +0530

    Added Code for Validation with useActionState Hook

---
+++ 21 Validation on Fields/3 Validation with useActionState Hook/App.jsx
@@ -0,0 +1,64 @@
+import { useState, useActionState } from "react";
+
+const nameRegex = /^[A-Za-z ]+$/;
+const emailRegex = /^[a-z0-9]+@[a-z]+.[a-z]+$/;
+
+export default function App() {
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  const validateData = (prevData, formData) => {
+    console.log("Name :", formData.get("name"));
+    console.log("password :", formData.get("password"));
+    console.log("Age :", formData.get("age"));
+    console.log("Email :", formData.get("email"));
+
+    const name = formData.get("name");
+    const password = formData.get("password");
+    const age = formData.get("age");
+    const email = formData.get("email");
+
+    if (name && password && age && email) {
+      if (!nameRegex.test(name)) {
+        console.log("Invalid Name");
+        setError("Invalid Name");
+        return {name, password, age, email};
+      } else if (!emailRegex.test(email)) {
+        console.log("Invalid Email");
+        setError("Invalid Email");
+        return {name, password, age, email};
+      } else {
+        setError(false);
+        setSuccess("Data Submitted Successfully");
+      }
+    } else {
+      console.log("Please Enter all the values");
+      setError("Please Enter all the values");
+      // alert("Please Enter all the values");
+      return {name, password, age, email};
+    }
+  };
+
+  const [data, action, pending] = useActionState(validateData);
+  console.log("Form Data ::::", data);
+
+  return (
+    <div className="App">
+      <h1>User Form</h1>
+      <form action={action}>
+        <input type="text" name="name" defaultValue={data?.name} placeholder="Enter User Name"/>
+        <br />
+        <input type="password" name="password" defaultValue={data?.password} placeholder="Enter Password"/>
+        <br />
+        <input type="number" name="age" defaultValue={data?.age} placeholder="Enter Age"/>
+        <br />
+        <input type="email" name="email" defaultValue={data?.email} placeholder="Enter Email ID"/>
+        <br />
+        <span style={{ color: "red" }}>{error ? error : null}</span>
+        <span style={{ color: "green" }}>{success ? success : null}</span>
+        <br />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
